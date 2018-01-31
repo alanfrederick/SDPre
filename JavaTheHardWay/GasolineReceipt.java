c95790f6d6c4b3c092349ef0e9dffba695cc22e0
alanfrederick <alan@alanfrederick.com>
2018-01-31 11:56:15 -0700

    Gas Receipt

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args)  {
+		System.out.println(  "+------------------------+"  );
+		System.out.println(  "|                        |"  );
+		System.out.println(  "|       CORNER STORE     |"  );
+		System.out.println(  "|                        |"  );
+		System.out.println(  "|  2018-01-30  05:09pm   |"  );
+		System.out.println(  "|                        |"  );
+		System.out.println(  "|  Gallons:       10.870 |"  );
+		System.out.println(  "|  Price/Gallon: $ 2.729 |"  );
+		System.out.println(  "|                        |"  );
+		System.out.println(  "|  Fuel total:  $ 29.66  |"  );
+		System.out.println(  "|                        |"  );
+		System.out.println(  "+------------------------+"  ); 
+	}
+}
