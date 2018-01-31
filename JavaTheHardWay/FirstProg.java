d404e25432141a7de863456fbcec966533ec795c
alanfrederick <alan@alanfrederick.com>
2018-01-31 11:49:00 -0700

    First Program

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+		public static void main( String[] args ) {
+			System.out.println( "I, Alan Frederick, am determined to learn how to code." );
+			System.out.println( "Today's date is January 30th, 2018." );
+		}
+}
