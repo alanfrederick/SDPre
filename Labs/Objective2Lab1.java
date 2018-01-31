b41fb418ea783787c7642dbf160a13e7202ec5aa
alanfrederick <alan@alanfrederick.com>
2018-01-31 13:11:47 -0700

    Objective 2 Lab 1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,15 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    String hello = "Hello World";
+
+    /*
+      System.out.println(hello);
+
+      Expected output in github is Hello<name> rather than
+      Hello World, below is that
+
+      String hello = "Hello Alan Frederick";
+      System.out.println(hello);
+    */
+  }
+}
