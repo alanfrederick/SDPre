4f676208750afcc09da7e89baf17cfd3545ab7e0
alanfrederick <alan@alanfrederick.com>
2018-01-31 12:11:35 -0700

    Objective 1 Lab 1

---
+++ Labs/Objective1Lab1.java
@@ -0,0 +1,18 @@
+public class Objective1Lab1{
+  public static void main(String[] args){
+
+
+    /*
+      System.out.println("Alan Frederick")
+
+    */
+
+    /*
+    lab1 expected output on github says Hello World and not my name
+    so I added a proper Hello World output to meet that condition
+    */
+
+    System.out.println("Hello World");
+
+  }
+}
